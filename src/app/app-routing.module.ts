0151aca82bd9a5004404f28daa0edb8391542ed3
Patrick Stracey <pstracey17@gmail.com>
2021-10-02 09:54:32 -0700

    init simple routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogFormComponent } from 'src/modules/blog/blog-form/blog-form.component';
+import { BlogIndexComponent } from 'src/modules/blog/blog-index/blog-index.component';
+import { BlogPostComponent } from 'src/modules/blog/blog-post/blog-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'post/:id/edit', component: BlogFormComponent },
+  { path: 'post/create', component: BlogFormComponent },
+  { path: 'post/:id', component: BlogPostComponent },
+  { path: '', component: BlogIndexComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
