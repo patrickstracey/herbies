6d9389ad0cba669c661d7243e554d659d722914a
Patrick Stracey <pstracey17@gmail.com>
2021-10-06 20:55:10 -0700

    copy updates & styles

--- src/modules/landing/storybook/storybook.data.ts
+++ src/modules/landing/storybook/storybook.data.ts
@@ -10,19 +10,21 @@ export const STORY_POINTS: StoryInterface[] = [
     buttonText: ' Our Story',
   },
   {
-    title: 'Forged in the peaks of the Sierra Nevada',
-    body: 'We started our journey on the banks of the mighty Truckee river just east of Lake Tahoe, where the cleansing mountain water and wacky Reno art scene gave us access to the outdoorsy-creative lifestyle our products embrace. ',
+    title: 'A Commitment to You & Mother Nature',
+    body: "We're focused on ensuring that we are a good stewards of both your experience and the greater industry as a whole. From supporting legalization and advocating for those hurt by the war on drugs. We're committed to making products that take us all higher.",
     image:
-      'https://cdn.myblackbird.com/0x,s1CGfKwyO8WsWVVYww9UhSgf16VVKRyAZ49lK7D6GZFU=/53/209c0660-abf3-11ea-a43b-9ddc8397d67c/Rythm-Energize-Cartridge-1587659112-1591887130.jpg',
+      'https://images.unsplash.com/photo-1538434375680-bd4599e71fa3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1552&q=80',
     url: '/blog',
     buttonText: 'Explore Products',
   },
   {
     title: 'Located in 150+ stores statewide',
-    body: 'We started our journey on the banks of the mighty Truckee river just east of Lake Tahoe, where the cleansing mountain water and wacky Reno art scene gave us access to the outdoorsy-creative lifestyle our products embrace. ',
+    body: "Whether you are in the Hollywood Hills or on your way out to the Burning Man playa, chances are you can find us nearby both in store and for delivery via blackbirdgo. We're just a few minutes away from wherever you are.",
     image:
-      'https://cdn.myblackbird.com/0x,s1CGfKwyO8WsWVVYww9UhSgf16VVKRyAZ49lK7D6GZFU=/53/209c0660-abf3-11ea-a43b-9ddc8397d67c/Rythm-Energize-Cartridge-1587659112-1591887130.jpg',
+      'https://images.unsplash.com/photo-1622704776938-bed6cd156e04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80',
     url: '/blog',
     buttonText: 'Find Us',
   },
 ];
+
+//'https://cdn.myblackbird.com/0x,s1CGfKwyO8WsWVVYww9UhSgf16VVKRyAZ49lK7D6GZFU=/53/209c0660-abf3-11ea-a43b-9ddc8397d67c/Rythm-Energize-Cartridge-1587659112-1591887130.jpg',
